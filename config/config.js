945f44623867e9d3b2244187230e3a16de4911e6
unknown <desde.rusia@hotmail.com>
2020-08-05 13:59:14 +0200

    Sección 12 terminada - Backend terminado

--- config/config.js
+++ config/config.js
@@ -1,1 +1,7 @@
 module.exports.SEED = '@este-es@un-seed-dificil';
+
+// =====================================
+// Google
+// =====================================
+module.exports.GOOGLE_CLIEN_ID = '876941548015-ausu2r143q4s4rcfq5dnd43k114tfskq.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = 'wkgq0GcJkj6Z-qq2IAwiNBdg';
